c0c07a1243e19b9d81c5b43c6cc2c00ba4f0150f
TruKave <brain.explosion05@gmail.com>
2017-05-31 17:01:33 +1200

    Updated player.png
    Updated it to fit new BG color.

--- Gam e.py
+++ Gam e.py
@@ -1,1 +1,22 @@
+# Made by TruKave Co. ()
+# Help from Jess & Seth
+
 import pygame
+import random
+import math
+import sys
+import os
+import time
+# OBJECTS:
+player = pygame.image.load(os.path.join('images', 'player.png')).convert
+
+# SETUP
+screenX = 160
+screenY = 90
+screen.fill('black')
+
+# MAIN LOOP/GAME
+while True:
+    time.sleep(1)
+    print("Test, yay!")
+    
