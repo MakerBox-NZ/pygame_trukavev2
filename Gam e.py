daa62cdf06d189d25b5250dc0eda71cf5b1f1e99
TruKave <brain.explosion05@gmail.com>
2017-08-02 17:27:38 +1200

    Added platforms! (DOESNT WORK)

--- Gam e.py
+++ Gam e.py
@@ -66,6 +66,48 @@ class Enemy(pygame.sprite.Sprite):
 
         self.counter += 1
 
+class Pillar(pygame.sprite.Sprite):
+    def __init__(self,x,y,img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', img))
+        self.image.convert_alpha()
+        self.image.set_colorkey(alpha)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.counter = 0
+    def move(self):
+        if self.counter >= 0 and self.counter <= 50:
+            self.rect.y -= 2
+        elif self.counter >= 50 and self.counter <= 100:
+            self.rect.y += 2
+        else:
+            self.counter = 0
+            print('reset')
+
+        self.counter += 1
+
+class Pillar0(pygame.sprite.Sprite):
+    def __init__(self,x,y,img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', img))
+        self.image.convert_alpha()
+        self.image.set_colorkey(alpha)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.counter = 0
+    def move(self):
+        if self.counter >= 0 and self.counter <= 50:
+            self.rect.y -= 2
+        elif self.counter >= 50 and self.counter <= 100:
+            self.rect.y += 2
+        else:
+            self.counter = 0
+            print('reset')
+
+        self.counter += 1
+
 class Ground(pygame.sprite.Sprite):
     def __init__(self,x,y,img):
         pygame.sprite.Sprite.__init__(self)
@@ -75,6 +117,30 @@ class Ground(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+class Static(pygame.sprite.Sprite):
+    #x location, y location, img width, img heighm img file
+    def __init__ (self, xloc, yloc, imgw, imgh, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([imgw, imgh])
+        self.image.convert_alpha()
+        self.image.set_colorkey(alpha)
+        self.blockpic = pygame.image.load(img).convert()
+        self.rect = self.image.get_rect()
+        self.rect.y = yloc
+        self.rect.x = xloc
+
+        #paint image into blocks
+        self.image.blit(self.blockpic, (0,0), (0,0,imgw,imgh))
+
+
+    def level1():
+        #create level 1
+        static_list = pygame.sprite.Group()
+        block = Static(0, 591, 768, 118,os.path.join('images', 'enemy.png'))
+        static_list.add(block) #after each block
+
+        return static_list.add(block) #at end of function level1
 
 # SETUP
 screenSize = [1920, 1080]
@@ -102,6 +168,8 @@ a = 0
 b = 0
 c = 0
 
+static_list = Static.level1() #set stage to Level 1
+
 player = Player() # Spawning player.
 player.rect.x = 100
 player.rect.y = 836
@@ -114,6 +182,16 @@ enemy = Enemy(500,836, 'enemy.png')
 enemy_list = pygame.sprite.Group()
 enemy_list.add(enemy)
 
+#Pillar code
+pillar = Pillar(500,772, 'pillar.png')
+pillar_list = pygame.sprite.Group()
+pillar_list.add(pillar)
+
+#Pillar0 code
+pillar0 = Pillar0(500,708, 'pillar.png')
+pillar0_list = pygame.sprite.Group()
+pillar0_list.add(pillar0)
+                        
 #ground code
 ground = Ground(0,900, 'line.png')
 ground_list = pygame.sprite.Group()
@@ -179,11 +257,17 @@ while main == True:
         redin = True
         print('red')
     screen.fill([a,b,c])
+
+    static_list.draw(screen) #draw platform on screen
     player.update(enemy_list)
     movingsprites.draw(screen)
     ground_list.draw(screen)
     enemy_list.draw(screen)
     enemy.move()
+    pillar1_list.draw(screen)
+    pillar1.move()
+    pillar0_list.draw(screen)
+    pillar0.move()
     pygame.display.flip()
     clock.tick(fps)
                 
